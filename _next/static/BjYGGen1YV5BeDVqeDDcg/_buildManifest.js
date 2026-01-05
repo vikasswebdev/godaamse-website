2389a565add1cf3a41c39d5e2dbc6ea814d06133
VIKAS PATEL <63722887+vikasswebdev@users.noreply.github.com>
2026-01-05 17:06:37 +0530

    Updates

---
+++ _next/static/BjYGGen1YV5BeDVqeDDcg/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/godaamse-website//_next/:path+",
+        "destination": "/godaamse-website/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
